82c0aae1f14b72e69637913f82624621148b2b0c
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-10-22 19:21:21 +0100

    Styling on coach chooser

--- src/app/components/CoachDetailsPage.tsx
+++ src/app/components/CoachDetailsPage.tsx
@@ -39,14 +39,28 @@ const CoachDetailsPage = ({ params }: Props) => {
 
 	return (
 		<div className="w-full h-full flex gap-5 text-slate-100 justify-start items-start flex-col transition-all duration-300 relative bg-slate-800">
-			<h2
-				className={`${loading ? "opacity-0" : "opacity-100"} transition-all duration-300 text-3xl sm:text-5xl w-fit h-fit absolute top-1/2 right-1/2 translate-x-1/2 z-10 text-nowrap`}
-			>
-				{name}
-			</h2>
 			<div className="flex lg:flex-row flex-col h-full w-full">
+				<div
+					className={`absolute top-0 left-0 w-full h-full rounded-br-full 
+            rounded-bl-full 
+            lg:rounded-bl-none 
+            lg:rounded-r-full shadow-2xl rounded-r-full ${transition ? "translate-x-[-200vw]" : "translate-x-0"} transition-all duration-300`}
+					style={{
+						background: `${id === coachData.details.length - 1 ? coachData.details[0].hex : coachData.details[id + 1].hex}`,
+					}}
+				/>
+				<div
+					className={`absolute top-0 left-0 lg:w-[98%] rounded-br-full 
+            rounded-bl-full 
+            lg:rounded-bl-none 
+            lg:rounded-r-full  w-full shadow-2xl lg:h-full h-[98%] rounded-r-full ${transition ? "translate-x-[-200vw]" : "translate-x-0"} transition-all duration-300`}
+					style={{
+						background: `${id === 0 ? coachData.details[coachData.details.length - 1].hex : coachData.details[id - 1].hex}`,
+					}}
+				/>
 				<section
 					className={`
+			relative	
             rounded-br-full 
             rounded-bl-full 
             lg:rounded-bl-none 
@@ -54,8 +68,10 @@ const CoachDetailsPage = ({ params }: Props) => {
             text-slate-100 
             p-8
             z-20
-            w-full 
-			h-full
+            lg:w-[96%] 
+			w-full
+			h-[96%]
+			lg:h-full
             ${transition ? "translate-x-[-200vw]" : "translate-x-0"} 
             transition-all 
             duration-300 
@@ -65,19 +81,51 @@ const CoachDetailsPage = ({ params }: Props) => {
 					<div
 						className={`gap-4 ${!firstPage ? "grid grid-cols-2 grid-rows-[1fr-1fr] w-full h-3/4 lg:h-full lg:w-3/4" : "flex flex-col justify-center items-center mt-5 w-full h-full"}`}
 					>
-						<div
-							className={`w-fit h-fit p-5 rounded-3xl flex flex-col gap-4 items-center justify-center ${firstPage ? "border border-slate-50 shadow-lg" : ""} justify-center items-center text-3xl`}
-						>
-							<div className="w-80 rounded-xl object-cover overflow-hidden">
-								<Image
-									className="rounded-xl saturate-100"
-									src={`${coachData.details[id].component_1.image_link}`}
-									alt={`${name} portrait`}
-									width={600}
-									height={600}
-								/>
+						<div className="flex flex-row justify-evenly">
+							{firstPage && (
+								<button
+									className={`rotate-180 ${loading ? "opacity-0" : "opacity-100"} transition-all duration-300 absolute left-10 top-1/2 translate-y-[-50%] translate-x-[-50%]`}
+									type="button"
+									onClick={previousCoach}
+								>
+									<Image
+										src="/icons/icons8-right-arrow-50.png"
+										alt="right arrow"
+										width={50}
+										height={50}
+									/>
+								</button>
+							)}
+							<div
+								className={
+									"w-fit h-fit p-5 rounded-3xl flex flex-col gap-4 items-center justify-center text-3xl"
+								}
+							>
+								<div className="w-80 rounded-xl object-cover overflow-hidden">
+									<Image
+										className="rounded-xl saturate-100"
+										src={`${coachData.details[id].component_1.image_link}`}
+										alt={`${name} portrait`}
+										width={600}
+										height={600}
+									/>
+								</div>
+								{name && firstPage && <h1>{name}</h1>}
 							</div>
-							{name && firstPage && <h1>{name}</h1>}
+							{firstPage && (
+								<button
+									type="button"
+									onClick={nextCoach}
+									className={`${loading ? "opacity-0" : "opacity-100"} transition-all duration-300 absolute right-0 top-1/2 translate-y-[-50%] translate-x-[-50%]`}
+								>
+									<Image
+										src="/icons/icons8-right-arrow-50.png"
+										alt="right arrow"
+										width={50}
+										height={50}
+									/>
+								</button>
+							)}
 						</div>
 						<div
 							className={`${!firstPage ? "h-full w-full flex justify-center items-start p-4" : ""}`}
@@ -133,37 +181,6 @@ const CoachDetailsPage = ({ params }: Props) => {
 						)}
 					</div>
 				</section>
-				{firstPage && (
-					<div
-						className="lg:w-[10%] w-full lg:h-full h-[10%] flex lg:flex-col flex-row gap-4 justify-evenly items-center"
-						style={{ filter: "invert(1)" }}
-					>
-						<button
-							className={`rotate-180 ${loading ? "opacity-0" : "opacity-100"} transition-all duration-300`}
-							type="button"
-							onClick={previousCoach}
-						>
-							<Image
-								src="/icons/icons8-right-arrow-50.png"
-								alt="right arrow"
-								width={50}
-								height={50}
-							/>
-						</button>
-						<button
-							type="button"
-							onClick={nextCoach}
-							className={`${loading ? "opacity-0" : "opacity-100"} transition-all duration-300`}
-						>
-							<Image
-								src="/icons/icons8-right-arrow-50.png"
-								alt="right arrow"
-								width={50}
-								height={50}
-							/>
-						</button>
-					</div>
-				)}
 			</div>
 		</div>
 	);
